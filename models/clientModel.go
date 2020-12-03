78eac1f7e62e69905a1d8b2244713d9ea2602200
Subham Panda <im.sspanda10@gmail.com>
2020-12-03 14:47:58 +0530

    feat: added scope to client model and GetClientByID function

--- models/clientModel.go
+++ models/clientModel.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"log"
 )
 
@@ -8,6 +9,7 @@ import (
 type Client struct {
 	ID string `db:"ID" json:"id"`
 	Name string `db:"NAME" json:"name"`
+	Scope string `db:"SCOPE" json:"scope"`
 }
 
 // ClientInit is used to initialize the clients table
@@ -16,7 +18,8 @@ func (DB *Database) ClientInit() {
 	_, err := DB.db.Exec(`
 		CREATE TABLE IF NOT EXISTS clients (
 			ID VARCHAR(256) PRIMARY KEY,
-			NAME VARCHAR(256) NOT NULL
+			NAME VARCHAR(256) NOT NULL,
+			SCOPE VARCHAR(256) NOT NULL
 		);
 	`)
 
@@ -24,4 +27,18 @@ func (DB *Database) ClientInit() {
 		log.Println(err)
 		log.Fatal("Error initializing clients Table")
 	}
+}
+
+// GetClientByClientID is a function that takes in clientID and returns back client
+func (DB *Database) GetClientByClientID(clientID string) (Client, error) {
+	query := fmt.Sprintf("SELECT * FROM clients WHERE id='%s'",clientID)
+
+	existingClients := []Client{}
+	err := DB.db.Select(&existingClients, query)
+
+	if (err != nil) || (err == nil && len(existingClients) == 0) {
+		return Client{}, err
+	}
+
+	return existingClients[0], nil
 }
